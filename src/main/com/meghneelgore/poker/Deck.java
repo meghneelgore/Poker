2f603ab5ab61f8bdda6902aaa35eaab4520c659f
Meghneel Gore <meghneelgore@gmail.com>
2018-03-06 22:16:07 -0800

    Made Deck backed by array instead of ArrayList

--- src/main/com/meghneelgore/poker/Deck.java
+++ src/main/com/meghneelgore/poker/Deck.java
@@ -1,25 +1,35 @@
 package com.meghneelgore.poker;
 
 
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Deck {
 
-    private ArrayList<Card> deck = new ArrayList<>(52);
+    private int currentDrawIndex = 0;
+    private Card[] deck = new Card[52];
 
     public Deck() {
         for (int i = 0; i < 52; i++) {
-            deck.add(i, new Card(Card.Suit.getSuit(i), Card.Rank.getRank(i % 13)));
+            deck[i] = new Card(Card.Suit.getSuit(i), Card.Rank.getRank(i % 13));
         }
+        currentDrawIndex = 0;
     }
 
     public void shuffle() {
-        Collections.shuffle(deck);
+        Random rnd = ThreadLocalRandom.current();
+        for (int i = deck.length - 1; i > 0; i--) {
+            int index = rnd.nextInt(i + 1);
+            // Simple swap
+            Card a = deck[index];
+            deck[index] = deck[i];
+            deck[i] = a;
+        }
+        currentDrawIndex = 0;
     }
 
 
     public Card draw() {
-        return deck.remove(deck.size() - 1);
+        return deck[currentDrawIndex++];
     }
 }
